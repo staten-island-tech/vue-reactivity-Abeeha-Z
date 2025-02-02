9f18e76df3f5a690bd90bfa321ca97451f775354
Abeeha-Z <abeehaz@nycstudents.net>
2025-02-01 19:26:28 -0500

    bjhbjh

---
+++ app/src/components/BakeryStore.vue
@@ -0,0 +1,21 @@
+
+<template>
+    <div>    
+    <li v-for="item in items">
+        {{ createCards() }}
+    </li>
+    </div>
+</template>
+
+
+<style scoped>
+</style>
+
+<script setup>
+methods: {
+    function createCards() {
+        
+    }
+}
+
+</script>

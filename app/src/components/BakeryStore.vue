62b9ecc3014941ed41bde953d8b6de921d2595f9
Abeeha-Z <abeehaz@nycstudents.net>
2025-02-09 11:17:41 -0500

    fdcsdf

--- app/src/components/BakeryStore.vue
+++ app/src/components/BakeryStore.vue
@@ -1,24 +1,19 @@
 <script setup>
 import items from './items'
-import ShoppingCart from './ShoppingCart.vue';
-
-methods: {
-
-  }
-
-
+import ShoppingCart from './ShoppingCart.vue'
 </script>
 
 <template>
-<div class="flex flex-wrap m-2 p-2 w-full h-auto ">
-  <itemcard class="w-32 text-wrap border-black border-2" v-for="item in items" :key="item.name"/>
-    <img :src="item.imageLink" class="w-32 h-48" />
-    <h2>
-      {{ item.name }}
-    </h2>
-    <p>{{ item.price }}</p>
-    <button @click="cart.push(item)">Add to Cart</button>
-</div>
+  <div class="flex flex-wrap m-2 p-2 w-full h-auto">
+    <div class="w-32 text-wrap border-black border-2" v-for="item in items" :key="item.name">
+      <img :src="item.imageLink" class="w-32 h-48" />
+      <h2>
+        {{ item.name }}
+      </h2>
+      <p>{{ item.price }}</p>
+      <button @click="cart_items.push(item)">Add to Cart</button>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
 import BakeryStore from '@/components/BakeryStore.vue'
+import ShoppingCart from '@/components/ShoppingCart.vue'
 </script>
 
 <template>
-    <BakeryStore/>
-    <!-- <ShoppingCart/> -->
-     <!-- <Header/> -->
+  <BakeryStore />
+  <ShoppingCart />
+  <!-- <Header/> -->
 </template>

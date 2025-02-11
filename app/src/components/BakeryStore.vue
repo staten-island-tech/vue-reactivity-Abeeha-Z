730005ef1e169b746fb277dee7602ea397103436
Abeeha-Z <158211569+Abeeha-Z@users.noreply.github.com>
2025-02-11 13:59:45 -0500

    tety

--- app/src/components/BakeryStore.vue
+++ app/src/components/BakeryStore.vue
@@ -1,17 +1,17 @@
 <script setup>
 import items from './items'
-import ShoppingCart from './ShoppingCart.vue'
+import ShoppingCart from './ShoppingCart.vue';
 </script>
 
 <template>
   <div class="flex flex-wrap m-2 p-2 w-full h-auto">
-    <div class="w-32 text-wrap border-black border-2" v-for="item in items" :key="item.name">
+    <div class="w-48 text-wrap border-black border-2 m-3 p-3" v-for="item in items" :key="item.name">
       <img :src="item.imageLink" class="w-32 h-48" />
       <h2>
         {{ item.name }}
       </h2>
       <p>${{ item.price }}</p>
-      <button class="border-black border-1" @click="cart_items.push(item)">Add to Cart</button>
+      <button class="border-black border-1" @click="add_toCart(item)">Add to Cart</button>
     </div>
   </div>
 </template>
--- app/src/components/ShoppingCart.vue
+++ app/src/components/ShoppingCart.vue
@@ -1,21 +1,33 @@
 <script setup>
-const cart_items = []
-function calcTotal(cart_item) {
-  total_price = 0
-  cart_items.forEach((cart_item)=> total_price.add(cart-items.price))
-}
+const cart_items = ref([]);
+let total_price = 0;
+
+function calcTotal() {
+  total_price = 0;
+  cart_items.forEach(cart_item => total_price += cart_item.price);
   console.log(total_price);
-  return total_price
+  return total_price;
+}
+
+function add_toCart(item) {
+  cart_items.value.push(item);
+  
+}
 </script>
 
 <template>
-  <div v-for="cart_item in cart_items" :key="cart_item.id" :cart_item="cart_item">
-    <div class="inline-flex">
-      <h1>{{cart_item.name}} ------ </h1>
-      <p>{{ cart_item.price }}</p>
+  <div class="fixed right-0 top-0 w-80 h-screen bg-gray-100 border-l border-gray-300 p-4">
+    <h2 class="text-xl font-semibold mb-4">Shopping Cart</h2>
+ 
+    <div v-for="cart_item in cart_items" :key="cart_item.id" class="mb-2 flex justify-between items-center border-b py-2">
+      <span class="text-sm font-medium">{{ cart_item.name }}</span>
+      <span class="text-sm text-gray-500">${{ cart_item.price }}</span>
     </div>
-
+    
+    <div class="mt-4 flex justify-between font-semibold text-lg">
+      <span>Total:</span>
+      <span>${{ calcTotal() }}</span>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
